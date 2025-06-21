b62b79ec1c795d153fd7c9a8354dd14cad6304af
santhoshs960 <186443900+santhoshs960@users.noreply.github.com>
2025-06-21 20:09:38 +0530

    Implement class

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // To store all book objects
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 
